15073553db75c4f573a0dd562912494391208852
Rai <raiaugustor@gmail.com.br>
2026-09-01 15:27:05 -0300

    Aula sobre Associação entre Classes

--- src/br/com/fiapride/model/Passageiro.java
+++ src/br/com/fiapride/model/Passageiro.java
@@ -28,34 +28,34 @@ public class Passageiro {
     public Passageiro(String nome, String cpf) {
         this.setNome(nome);
         this.setCpf(cpf);
-        this.setSaldo(0);
+        this.setSaldo(0.0);
     }
 
-        public double getSaldo() {
-            return this.saldo;
-        }
+    public double getSaldo() {
+        return this.saldo;
+    }
 
-        private void setSaldo(double valor) {
-            if (valor >= 0) {
-                this.saldo = valor;
-            } else {
-                System.out.println("Erro de Segurança: Tentativa de definir saldo negativo bloqueada!");
-            }
+    private void setSaldo(double valor) {
+        if (valor >= 0) {
+            this.saldo = valor;
+        } else {
+            System.out.println("Erro de Segurança: Tentativa de definir saldo negativo bloqueada!");
         }
+    }
 
-        public String getNome() {
-            return this.nome;
-        }
+    public String getNome() {
+        return this.nome;
+    }
 
-        private void setNome(String nome) {
-            this.nome = nome;
-        }
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
 
-        public String getCpf() {
-            return this.cpf;
-        }
+    public String getCpf() {
+        return this.cpf;
+    }
 
-        private void setCpf(String cpf) {
-            this.cpf = cpf;
-        }
+    private void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
--- src/br/com/fiapride/model/SistemaPrincipal.java
+++ src/br/com/fiapride/model/SistemaPrincipal.java
@@ -1,6 +1,8 @@
 package br.com.fiapride.model;
 
 import br.com.fiapride.model.Passageiro;
+import br.com.fiapride.model.Viagem;
+import br.com.fiapride.model.Veiculo;
 
 public class SistemaPrincipal {
 
@@ -8,23 +10,15 @@ public class SistemaPrincipal {
 
         System.out.println("--- Iniciando o Sistema FiapRide --- \n");
 
-        Passageiro passageiro1 = new Passageiro("Ana Silva", "222.222.222-22");
-        System.out.println(">>> Recarga Passageiro 1:");
-        passageiro1.adicionarSaldo(50.0);
+        Passageiro ana = new Passageiro("Ana Silva", "222.222.222-22");
+        Veiculo carroDoJoao = new Veiculo("ABC-1234", "Toyota Corolla0");
 
-        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333.333.333-33");
-        System.out.println(">>> Recarga Passageiro 2:");
-        passageiro2.adicionarSaldo(12.5);
+        Viagem viagemDaAna = new Viagem("Avenida Paulista, 1000", ana, carroDoJoao);
 
-        System.out.println("\n--- Status dos Passageiros ---");
-        System.out.println("Passageiro: " + passageiro1.getNome() + " | Saldo: R$ " + passageiro1.getSaldo() + " | CPF: " + passageiro1.getCpf());
-        System.out.println("Passageiro: " + passageiro2.getNome() + " | Saldo: R$ " + passageiro2.getSaldo() + " | CPF: " + passageiro2.getCpf());
+        viagemDaAna.exibirResumo();
 
-        System.out.println("\n--- Realizando Viagens ---");
-        System.out.println("Pagando viagem do passageiro 1 (Ana)...");
-        passageiro1.pagarViagem(20.0);
+        ana.adicionarSaldo(50.0);
 
-        System.out.println("\nPagando viagem do passageiro 2 (Carlos)...");
-        passageiro2.pagarViagem(20.0);
+        System.out.println("Saldo da ana consultado ATRAVÉS da viagem: R$: " + viagemDaAna.getSolicitante().getSaldo());
     }
 }
